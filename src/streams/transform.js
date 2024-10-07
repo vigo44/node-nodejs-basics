9b7e37e3e64e9908766d4a181f9a5108fac757f3
vigo44 <vvigo@rambler.ru>
2024-10-07 23:07:43 +0400

    feat: resolve streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { stdin, stdout } from "node:process";
+import { Transform } from "stream";
+import { pipeline } from "stream/promises";
+
 const transform = async () => {
-    // Write your code here 
+  const reverseStream = new Transform({
+    transform(chunk, _, callback) {
+      const reverseChunk = chunk.toString().trim().split("").reverse().join("");
+      callback(null, `${reverseChunk}\n`);
+    },
+  });
+  pipeline(stdin, reverseStream, stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { createWriteStream } from "node:fs";
+import { stdin } from "node:process";
+
 const write = async () => {
-    // Write your code here 
+  const fileName = fileURLToPath(import.meta.url);
+  const dirName = dirname(fileName);
+  const sourcePath = join(dirName, "files", "fileToWrite.txt");
+  const sourceStream = createWriteStream(sourcePath);
+  stdin.on("data", (data) => {
+    sourceStream.write(data);
+  });
 };
 
 await write();
