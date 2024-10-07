b1816dfee423d0ebe14cd3eb17dcff3847904b2f
vigo44 <vvigo@rambler.ru>
2024-10-07 23:34:50 +0400

    feat: resolve zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { createGzip } from "node:zlib";
+import { pipeline } from "stream/promises";
+import { createReadStream, createWriteStream } from "node:fs";
+
 const compress = async () => {
-    // Write your code here 
+  const fileName = fileURLToPath(import.meta.url);
+  const dirName = dirname(fileName);
+  const fileToCompressPath = join(dirName, "files", "fileToCompress.txt");
+  const archivePath = join(dirName, "files", "archive.gz");
+  const readStream = createReadStream(fileToCompressPath);
+  const writeStream = createWriteStream(archivePath);
+  const gzip = createGzip();
+  await pipeline(readStream, gzip, writeStream);
 };
 
 await compress();
